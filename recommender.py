27b2cb8d619d33253e657ab9f7829d7bde5d6a5a
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2023-10-25 22:12:12 +0200

    Got recommender up and running, started dockerfile

--- recommender.py
+++ recommender.py
@@ -1,10 +1,55 @@
+import pickle
+import numpy as np
 from fastapi import FastAPI
-from pathlib import Path
 from db_utils import psqldb
 
-# global settings
-QPATH = Path("queries")
-DB = psqldb("pagila")
+# Connect to database
+postgres_db = psqldb("pagila")
+
+# read model
+with open("model/tfidf_transformer.pkl", "rb") as trf_file:
+    tfidf_transformer = pickle.load(trf_file)
+with open("model/model.pkl", "rb") as mod_file:
+    model = pickle.load(mod_file)
+
+def get_past_rentals(customer_id: int) -> list[int]:
+    result = postgres_db.run_query(
+        f"""
+        select inventory.film_id 
+        from rental 
+        left join inventory 
+        on rental.inventory_id=inventory.inventory_id
+        where rental.customer_id={customer_id}
+        """
+    )
+    return result[:,0].to_list()
+
+def get_film_information(film_ids: list[int]) -> list[dict]:
+    sql_ref = tuple(film_ids) if len(film_ids) > 1 else f"({film_ids[0]})"
+    result = postgres_db.run_query(
+        f"""
+        select film_id, title, release_year as year, description, rating 
+        from film 
+        where film_id in {sql_ref}
+        """
+    )
+    return result.to_dicts()
+
+
+def get_recommendations(articles_bought: list[int], n_rec=5) -> list[int]:
+    x = np.zeros(1000, int)
+    for a in articles_bought:
+        x[a - 1] += 1
+    w = model.transform(tfidf_transformer.transform([x]))
+    xhat = model.inverse_transform(w)
+    rank = xhat.argsort()[0] + 1
+    rank = rank.tolist()
+    recs: list[int] = []
+    while len(recs) < n_rec:
+        item = rank.pop()
+        if not item in articles_bought:
+            recs.append(item)
+    return recs
 
 # init fastapi
 app = FastAPI()
@@ -15,10 +60,16 @@ def read_root():
 
 @app.get("/customers/{customer_id}")
 def get_customer_by_id(customer_id: int):
-    return DB. \
-        run_query(f"select * from customer where customer_id={customer_id}"). \
-        to_dicts()
+    return postgres_db.run_query(
+        f"""
+        select * 
+        from customer 
+        where customer_id={customer_id}
+        """
+    ).to_dicts()
 
-
-# res = db.run_sql_file(QPATH / "alltables.sql")
-# res.filter(pl.col("schemaname") == "webshop").select(pl.col("tablename")).to_numpy()
+@app.get("/recommend/{customer_id}")
+def recommend(customer_id: int, n: int = 5):
+    past_rentals = get_past_rentals(customer_id)
+    recs = get_recommendations(past_rentals, n_rec=n)
+    return get_film_information(recs)
--- train_model.py
+++ train_model.py
@@ -1,11 +1,15 @@
-from pathlib import Path
+
+import warnings
+import pickle
 from db_utils import psqldb
 import polars as pl
 import numpy as np
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.decomposition import NMF, TruncatedSVD
-import pickle
+from sklearn.decomposition import TruncatedSVD
 import matplotlib.pyplot as plt
+
+# turn off warning for conversion without column names
+warnings.filterwarnings(action='ignore', category=UserWarning)
 
 # set up database connection and run basic query
 db = psqldb("pagila")
@@ -26,34 +30,49 @@ full_df = \
     join(rentals, on=["customer_id", "film_id"], how="left"). \
     fill_null(0)
 
+
 # pivot wider to create a count matrix
 rental_matrix = full_df.pivot(
-    values="n", 
-    index="customer_id", 
-    columns="film_id", 
+    values="n",
+    index="customer_id",
+    columns="film_id",
     aggregate_function="sum"
 )
 
-
-
 # transform to tfidf matrix
-transformer = TfidfTransformer()
-rental_matrix_tfidf = transformer.fit_transform(rental_matrix[:,1:])
+tfidf_transformer = TfidfTransformer()
+rental_matrix_tfidf = tfidf_transformer.fit_transform(rental_matrix[:,1:])
 
-# estimate nonnegative matrix factorization model
-model = TruncatedSVD(n_components=2)
+# Latent semantic analysis
+model = TruncatedSVD(n_components=50)
 model.fit(rental_matrix_tfidf)
 
-W = model.transform(rental_matrix_tfidf)
-H = model.components_
+# store model
+with open("model/tfidf_transformer.pkl", "wb") as trf_file:
+    pickle.dump(tfidf_transformer, file=trf_file)
+with open("model/model.pkl", "wb") as mod_file:
+    pickle.dump(model, file=mod_file)
 
-model.inverse_transform(W[1,:].reshape(1, -1)).argmax()
-
-model.inverse_transform([[0,0]])
-
-def get_recommendation(articles_bought: list[int]):
+# test model
+def get_recommendation(articles_bought: list[int], n_rec=5):
     x = np.zeros(1000, int)
     for a in articles_bought:
         x[a - 1] += 1
-    model.inverse_transform(model.transform(transformer.transform([x]))).argsort() + 1
+    w = model.transform(transformer.transform([x]))
+    xhat = model.inverse_transform(w)
+    rank = xhat.argsort()[0].tolist()
+    rank += 1
+    recommendations: list[int] = []
+    while len(recommendations) < n_rec:
+        item = rank.pop()
+        if not item in articles_bought:
+            recommendations.append(item)
 
+    return recommendations # last 5 items in reverse order
+
+
+model.inverse_transform(model.transform(rental_matrix_tfidf))
+model.explained_variance_ratio_.cumsum()
+
+get_recommendation([1, 45, 323])
+get_recommendation(np.where(rental_matrix[0,1:])[1]+1)
